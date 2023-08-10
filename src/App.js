c6ffc5fdbeab43dc99d778107a4901dd690fe5a9
iglesiaskevinralph <iglesiaskevinralph@gmail.com>
2023-08-10 19:02:39 +0800

    update app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Admin from './pages/Admin';
+
+const socket = io.connect('https://crescendo-maze-socket.onrender.com');
 
 function App() {
-
+  const [username, setUsername] = useState('');
+  const [selectedChar, setSelectedChar] = useState('adventurer01');
 
   return (
-    <div>RUNNING</div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                selectedChar={selectedChar}
+                setSelectedChar={setSelectedChar}
+                socket={socket}
+              />
+            }
+          />
+          <Route path='/game' element={
+              <Game
+                username={username}
+                selectedChar={selectedChar}
+                socket={socket}
+                role="player"
+              />
+            }
+          />
+          <Route path='/admin' element={
+              <Admin socket={socket} />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
